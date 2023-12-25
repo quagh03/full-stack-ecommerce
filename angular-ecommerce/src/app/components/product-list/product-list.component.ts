f05fc70b99c56c47fe23701dc1bb8bd5059295da
quagh03 <03.quanghuy.le@gmail.com>
2023-12-25 12:06:21 +0700

    init

---
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {Product} from "../../common/product";
+import {CurrencyPipe, NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [
+    NgForOf,
+    CurrencyPipe
+  ],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent implements OnInit{
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.listProducts();
+  }
+
+  listProducts(){
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+}
